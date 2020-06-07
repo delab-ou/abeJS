02818bfc8566ca60fdf4c1b069de6fed615239f1
Hideharu Kojima <hal@HideharunoMacBook-Pro.local>
2020-06-08 01:32:12 +0900

    change

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 
 console.log("test");
 const napi = require("bindings")("napi-abe")
-/*
-const abe= new napi.JsOABE(
-  "KP-ABE",
-  "AAAAFqpvymHEZStUvLHPjRcdZgPnTKFtcGsAAAGooQFZsgEEtLIBACF0Esx3MYgYNRQrnYDMBR8VZtC4znBzEN96jO4+FX5aETzJYSNcya9AvUWkfgD4+OquQhL88VTA8yWRfaWx3WAkoLQO3RALMz1GLxPCchmvY4g6oyGMbM4bfzXfNakHcgY0dQIeF2911Mhh/AgqtC8DW84Xq/jqrZXfH8Fkjp1WEYQUU13l4DcbnSSXX1a4eRo6ZRxqbx2+Rex3o+bQW78JXtc9jq1xhhxa2V6xHSqrGX8HNST/Wrjr6cbhrDV2zR6PK+3TDt7j30HowNFylFF9sLSJxWdejG0Byfq5yMYVIKDcjHalzfQbv9xLF2aoVBzn390B9ezLFQuvmVm8pd6hAmcxoSSyoSECAAqmZte+VbSUIhRp34gudrbx0tTT+NGsj7cg/06H2yahAmcyoUSzoUECG2VYdCmhbPUIxJVXZgpnBgqF3K2d8ACTjpycIE2+gyQbZ34R6kFBnsJnikUlbcD2jUqSRWqpu8nUrs+bm/sqRqEBa6ElHQAAACAd1GaXC0c5zz8+AH7ZjKO3IiFDh5JlVK5iXSPL/2xEGg==",
-  "AAAAFqpvyhAhFWLAa3rKDTuV74D3m0Rtc2sAAAAooQF5oSOxACAQYNZLp27RymhkyhQd5jwacUhUBSEHKdDmV8yMX1VlsA==");
-*/
-
 var fs = require('fs');
 var keys = JSON.parse(fs.readFileSync('./keys.json'));
 var abetype="KP-ABE";
@@ -20,28 +13,8 @@ var ctext;
 var ptext="text1";
 var ptext2="text2";
 
-var msk=abe.exportMSK();
-var mpk=abe.exportMPK();
 console.log("msk = \n\""+msk+"\"");
 console.log("mpk = \n\""+mpk+"\"");
-/*
-var masterkeys = {
-    mpk: mpk,
-    msk: msk,
-};
-fs.writeFile('keys.json', JSON.stringify(masterkeys),function(err, result) {
-        if(err) console.log('error', err);
-});
-
-var json = JSON.parse(fs.readFileSync('./keys.json'));
-
-console.log(json);
-console.log("msk:"+json['msk']);
-console.log("mpk:"+json['mpk']);
-*/
-//abe.importMPK(mpk);
-//abe.importMSK(msk);
-
 abe.keygen("(attr1 or attr2)", "key0");
 abe.keygen("(attr3 and attr4)","key1");
 abe.keygen("(attr3 or attr5)","key2");
